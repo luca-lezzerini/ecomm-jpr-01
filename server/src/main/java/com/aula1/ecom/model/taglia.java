ce09100b79984a2c980baf80a4ea9e9afb447f5d
gianpolonio <67907519+gianpolonio@users.noreply.github.com>
2020-08-04 11:26:55 +0200

    Revert "Collegare Spring al DB #13 aggiornamento 2"
    This reverts commit 29a602148facf9a7a04a786a8b225116c5417dc0.

---
+++ server/src/main/java/com/aula1/ecom/model/taglia.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aula1.ecom.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Taglia")
+public class Taglia implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "descrizione")
+    private String descrizione;
+    @Column(name = "sigla")
+    private String sigla;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public Taglia(Long id, String descrizione, String sigla) {
+        this.id = id;
+        this.descrizione = descrizione;
+        this.sigla = sigla;
+    }
+
+    public Taglia() {
+    }
+
+    @Override
+    public String toString() {
+        return "Taglia{" + "id=" + id + ", descrizione=" + descrizione + ", sigla=" + sigla + '}';
+    }
+
+}
