8f1ed3134f4d531468b671e79cc09afda71cfc1f
rdef98 <r.defilippis@accademiainformatica.com>
2020-08-04 11:25:21 +0200

    Revert "Collegare Spring al DB #13 aggiornamento 2"
    This reverts commit 29a602148facf9a7a04a786a8b225116c5417dc0.

---
+++ server/src/test/java/com/aula1/ecom/EcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula1.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
