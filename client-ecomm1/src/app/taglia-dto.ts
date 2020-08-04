da2095bc6372c41d2afc07e1c341d197cb11ffa8
rdef98 <r.defilippis@accademiainformatica.com>
2020-08-04 16:01:00 +0200

    Sviluppare app.component.ts (Taglia) #21 - Aggiornamento 1
    Prima prova controller Angular per taglie.

--- client-ecomm1/src/app/taglia-dto.ts
+++ client-ecomm1/src/app/taglia-dto.ts
@@ -1,10 +1,6 @@
-export class Taglia {
-    id: number;
-    descrizione: string;
-    sigla: string;
-    constructor(id: number, descrizione: string, sigla: string) {
-        this.id = id;
-        this.descrizione = descrizione;
-        this.sigla = sigla;
+export class TagliaDto {
+    taglia: string;
+    constructor(taglia: string) {
+        this.taglia = taglia;
     }
 }
