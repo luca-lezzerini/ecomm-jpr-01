53d174db52e2fd1fd62bcb9eccd9972081b22459
rdef98 <r.defilippis@accademiainformatica.com>
2020-08-05 10:56:56 +0200

    Import CRUDTaglia

--- client-ecomm1/src/app/app.module.ts
+++ client-ecomm1/src/app/app.module.ts
@@ -6,13 +6,13 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CRUDColoriComponent } from './crudcolori/crudcolori.component';
-import { CrudtagliaComponent } from './crudtaglia/crudtaglia.component';
+import { CRUDTagliaComponent } from './crudtaglia/crudtaglia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CRUDColoriComponent,
-    CrudtagliaComponent
+    CRUDTagliaComponent
   ],
   imports: [
     BrowserModule,
