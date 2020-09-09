afb9b90838c38fca69e1050fd3edf99b36793766
skrunex <67907611+skrunex@users.noreply.github.com>
2020-09-09 15:15:00 +0200

    ultimato prodotto

--- server/src/main/java/com/aula1/ecom/model/Prodotto.java
+++ server/src/main/java/com/aula1/ecom/model/Prodotto.java
@@ -5,10 +5,83 @@
  */
 package com.aula1.ecom.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author Kevin
  */
+@Entity
 public class Prodotto {
-    
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String codice;
+    @Column
+    private String descrizione;
+    @Column
+    private Double peso;
+    @Column
+    private Double prezzo;
+
+    //COSTRUTTORI
+    public Prodotto(Long id, String codice, String descrizione, Double peso, Double prezzo) {
+        this.id = id;
+        this.codice = codice;
+        this.descrizione = descrizione;
+        this.peso = peso;
+        this.prezzo = prezzo;
+    }
+
+    public Prodotto() {
+    }
+
+    //METODI GET AND SETTER
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public void setCodice(String codice) {
+        this.codice = codice;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(Double prezzo) {
+        this.prezzo = prezzo;
+    }
+
 }
