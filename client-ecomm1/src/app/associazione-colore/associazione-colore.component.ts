841801310203c053956d6e74b91b6ea143212221
Prisoscar <67907720+Prisoscar@users.noreply.github.com>
2020-09-09 17:20:09 +0200

    colore angular

--- client-ecomm1/src/app/associazione-colore/associazione-colore.component.ts
+++ client-ecomm1/src/app/associazione-colore/associazione-colore.component.ts
@@ -1,3 +1,7 @@
+import { Observable } from 'rxjs';
+import { CercaDto } from './../crudtaglia/cerca-dto';
+import { MemoriaCondivisaService } from './../memoria-condivisa-service';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssociazioneColoreComponent implements OnInit {
 
-  constructor() { }
+  criterioRicerca = "";
+  listaProdotto
+  constructor(private http: HttpClient, private memoriaCondivisa: MemoriaCondivisaService) { }
 
   ngOnInit(): void {
   }
+
+  cerca() {
+    /*let cercaDto = new CercaDto(this.criterioRicerca, this.memoriaCondivisa.token);
+    let b: Observable<ListaProdottoDto> =
+      this.http.
+        post<ListaProdottoDto>(this.urlHost + "/cercaColore", cercaDto);
+    let ss: Subscription = b.subscribe(
+      c => {
+        this.listaColore = c.listaColori;
+        this.memoriaCondivisa.token = c.token;
+      }
+  }*/
 
 }
