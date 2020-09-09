0346061e4534074c7b627b643c636e9d9472520a
silviacasa68 <silviacasagrande1968@gmail.com>
2020-09-09 15:47:52 +0200

    associazione colore

---
+++ client-ecomm1/src/app/associazione-colore/associazione-colore.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-associazione-colore',
+  templateUrl: './associazione-colore.component.html',
+  styleUrls: ['./associazione-colore.component.css']
+})
+export class AssociazioneColoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
